47b8c4e264267d177dbf9a7ca576bc6c78cd9821
yektaleon <117384510+yektaleon@users.noreply.github.com>
2023-01-22 17:04:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,10 +18,10 @@ class SiCalendar(MycroftSkill):
         # student user name
         user_name = "test"
         
-        username = self.settings.get('username')
+        username = self.settings.get('username', 'yk020@hdm-stuttgart.de')
         if not self.username:
            self.log.info('Failed to retrieve username')
-        password = self.settings.get('password')
+        password = self.settings.get('password', 'SIPasswortyk020')
         if not self.password:
            self.log.info('Failed to retrieve password')
         
